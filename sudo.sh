8022fab13a599153c392df374b28ef1bd5b2f748
dekken <philip.deegan@gmail.com>
2016-10-09 14:06:19 +0100

    init

---
+++ sudo.sh
@@ -0,0 +1,7 @@
+
+
+cd split
+cat "lxc1.0.8_x86_64_centos_7.2-6.7.tar.bz2_split"* > lxc1.0.8_x86_64_centos_7.2-6.7.tar.bz2
+mv lxc1.0.8_x86_64_centos_7.2-6.7.tar.bz2 ..
+cd ..
+tar xf lxc1.0.8_x86_64_centos_7.2-6.7.tar.bz2
